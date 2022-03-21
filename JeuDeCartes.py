0aadb3c368cd7ead0fdb0322a005390666f8f3e2
Kosoca <59687626+Kosoca@users.noreply.github.com>
2022-03-21 16:00:39 +0100

    Update JeuDeCartes.py

--- JeuDeCartes.py
+++ JeuDeCartes.py
@@ -1,1 +1,36 @@
 
+from JeuDeCartes import JeuDeCartes
+from Player import Player
+
+player2 = Player(JeuDeCartes())
+player1 = Player(JeuDeCartes())
+
+player2.getDeck().battre()
+player1.getDeck().battre()
+
+print(player1.getDeck().getCardsGame())
+print("=-----------------=")
+print(player2.getDeck().getCardsGame())
+
+nbEgalite = 0
+print("=-----------------=")
+for count in range(0, 52):
+    tirageP1 = player1.getDeck().tirer()[0]
+    tirageP2 = player2.getDeck().tirer()[0]
+    if (tirageP1 == tirageP2):
+        nbEgalite = nbEgalite + 1
+        pass
+
+    if (tirageP1 < tirageP2):
+        player2.addPoint()
+        print(tirageP1, tirageP2)
+        pass
+    else:
+        player1.addPoint()
+        print(tirageP1, tirageP2)
+        pass
+    pass
+
+print(f"Joueur 1 : {player1.getPoints()} point(s)")
+print(f"Joueur 2 : {player2.getPoints()} point(s)")
+print(f"égalités : {nbEgalite}")
